bf4d1b6facc077471c687c27b7ef9fafd0cb73e8
HarshadSanjayKarale <harshadkarale0@gmail.com>
2024-02-17 23:24:01 +0530

    todo list done

--- src/components/ModifierBoard/index.jsx
+++ src/components/ModifierBoard/index.jsx
@@ -39,6 +39,8 @@ const ModifierBoard = ({
 
   const [openMood, setOpenMood] = useState(false);
   const [openFocus, setOpenFocus] = useState(false);
+  const [openTodo, setOpenTodo] = useState(false);
+  const [todoList, setTodoList] = useState([]);
 
   const [cityTraffic, setCityTraffic] = useState(0);
   const [cityRain, setCityRain] = useState(0);
@@ -71,6 +73,9 @@ const ModifierBoard = ({
     setOpenMood(!openMood);
     setOpenFocus(false);
   };
+  const addTodo = (todo) => {
+    setTodoList([...todoList, todo]);
+  };
 
   const changeMoodHandler = (e) => {
     dispatch(changeMoodStatus(e.currentTarget.id));
@@ -85,25 +90,25 @@ const ModifierBoard = ({
       {!openMood && (
         <div>
           <ReactAudioPlayer
-            preload='auto'
+            preload="auto"
             autoPlay
-            src='./assets/musics/city_traffic.mp3'
+            src="./assets/musics/city_traffic.mp3"
             loop
             volume={cityTraffic / 100}
           />
 
           <ReactAudioPlayer
-            preload='auto'
+            preload="auto"
             autoPlay
-            src='./assets/musics/fireplace.mp3'
+            src="./assets/musics/fireplace.mp3"
             loop
             volume={fireplace / 100}
           />
 
           <ReactAudioPlayer
-            preload='auto'
+            preload="auto"
             autoPlay
-            src='./assets/musics/rain_city.mp3'
+            src="./assets/musics/rain_city.mp3"
             loop
             volume={rainValue / 100}
           />
@@ -111,236 +116,239 @@ const ModifierBoard = ({
       )}
       <div
         className={
-          `modifier ` + (openMood && "mood ") + (openFocus && " focus ")
+          `modifier ` +
+          (openMood && "mood ") +
+          (openFocus && " focus ") +
+          (openTodo && "todo")
         }
       >
-        <div className='modifier__icon'>
+        <div className="modifier__icon">
           <div className={`icon ${openMood && "active"}`}>
-            <i onClick={openMoodHandler} className='fas fa-sliders-h fa-2x'></i>
+            <i onClick={openMoodHandler} className="fas fa-sliders-h fa-2x"></i>
           </div>
           {openMood && (
-            <div className='modifierBox'>
+            <div className="modifierBox">
               <h4>Mood</h4>
-              <div className='options'>
+              <div className="options">
                 <div
-                  id='sleep'
+                  id="sleep"
                   onClick={changeMoodHandler}
                   className={`item ${moodMode === "sleep" ? "active" : ""}`}
                 >
-                  <i id='sleep' className='fas fa-moon fa-2x'></i>
-                  <span id='sleep'>Sleep</span>
+                  <i id="sleep" className="fas fa-moon fa-2x"></i>
+                  <span id="sleep">Sleep</span>
                 </div>
                 <div
-                  id='jazzy'
+                  id="jazzy"
                   onClick={changeMoodHandler}
                   className={`item ${moodMode === "jazzy" ? "active" : ""}`}
                 >
-                  <i id='jazzy' className='fas fa-guitar fa-2x'></i>
-                  <span id='jazzy'>Jazzy</span>
+                  <i id="jazzy" className="fas fa-guitar fa-2x"></i>
+                  <span id="jazzy">Jazzy</span>
                 </div>
                 <div
-                  id='chill'
+                  id="chill"
                   onClick={changeMoodHandler}
                   className={`item ${moodMode === "chill" ? "active" : ""}`}
                 >
-                  <i id='chill' className='fas fa-coffee fa-2x'></i>
-                  <span id='chill'>Chill</span>
+                  <i id="chill" className="fas fa-coffee fa-2x"></i>
+                  <span id="chill">Chill</span>
                 </div>
               </div>
-              <div className='volume'>
+              <div className="volume">
                 <Stack
                   spacing={2}
-                  direction='row'
+                  direction="row"
                   sx={{ mb: 1 }}
-                  alignItems='center'
+                  alignItems="center"
                 >
-                  <i className='fas fa-volume-down fa-lg'></i>
+                  <i className="fas fa-volume-down fa-lg"></i>
                   <Slider
-                    className='volume-slider'
+                    className="volume-slider"
                     value={volumeValue}
                     onChange={changeVolumeHandler}
                   />
-                  <i className='fas fa-volume-up fa-lg'></i>
+                  <i className="fas fa-volume-up fa-lg"></i>
                 </Stack>
               </div>
               <h5>Background Noise</h5>
-              <div className='backgroundNoise'>
-                <div className='noise-option'>
+              <div className="backgroundNoise">
+                <div className="noise-option">
                   <p>City traffic</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/city_traffic.mp3'
+                    src="./assets/musics/city_traffic.mp3"
                     loop
                     volume={cityTraffic / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={cityTraffic}
                     onChange={(e) => setCityTraffic(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>City rain</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/rain_city.mp3'
+                    src="./assets/musics/rain_city.mp3"
                     loop
                     volume={rainValue / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={rainValue}
                     onChange={rainSliderHandler}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Fireplace</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/fireplace.mp3'
+                    src="./assets/musics/fireplace.mp3"
                     loop
                     volume={fireplace / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={fireplace}
                     onChange={(e) => setFireplace(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Snow</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/snow.mp3'
+                    src="./assets/musics/snow.mp3"
                     loop
                     volume={snow / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={snow}
                     onChange={(e) => setSnow(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Summer Storm</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/summer_storm.mp3'
+                    src="./assets/musics/summer_storm.mp3"
                     loop
                     volume={summerStorm / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={summerStorm}
                     onChange={(e) => setSummerStorm(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Fan</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/fan.mp3'
+                    src="./assets/musics/fan.mp3"
                     loop
                     volume={fan / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={fan}
                     onChange={(e) => setFan(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Forest Night</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/forest_night.mp3'
+                    src="./assets/musics/forest_night.mp3"
                     loop
                     volume={forestNight / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={forestNight}
                     onChange={(e) => setForestNight(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Wave</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/waves.mp3'
+                    src="./assets/musics/waves.mp3"
                     loop
                     volume={wave / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={wave}
                     onChange={(e) => setWave(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Wind</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/wind.mp3'
+                    src="./assets/musics/wind.mp3"
                     loop
                     volume={wind / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={wind}
                     onChange={(e) => setWind(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>People</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/people_talk_inside.mp3'
+                    src="./assets/musics/people_talk_inside.mp3"
                     loop
                     volume={people / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={people}
                     onChange={(e) => setPeople(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>River</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/river.mp3'
+                    src="./assets/musics/river.mp3"
                     loop
                     volume={river / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={river}
                     onChange={(e) => setRiver(e.target.value)}
                   />
                 </div>
-                <div className='noise-option'>
+                <div className="noise-option">
                   <p>Rain Forest</p>
                   <ReactAudioPlayer
-                    preload='auto'
+                    preload="auto"
                     autoPlay
-                    src='./assets/musics/rain_forest.mp3'
+                    src="./assets/musics/rain_forest.mp3"
                     loop
                     volume={rainForest / 100}
                   />
                   <Slider
-                    className='slider'
+                    className="slider"
                     value={rainForest}
                     onChange={(e) => setRainForest(e.target.value)}
                   />
@@ -349,32 +357,33 @@ const ModifierBoard = ({
             </div>
           )}
         </div>
-        <div className='modifier__icon'>
+        <div className="modifier__icon">
           <div className={`icon ${openFocus && "active"}`}>
             <i
               onClick={openFocusHandler}
-              className='fas fa-book-reader fa-2x'
+              className="fas fa-book-reader fa-2x"
             ></i>
           </div>
         </div>
         {openFocus && (
-          <div className='modifierBox'>
-            <PomodoroApp/>
-            {/* <h4>Focus Mode</h4>
-            <CountDownTimer
-              seconds={seconds}
-              minutes={minutes}
-              hours={hours}
-              isRunning={isRunning}
-              pause={pause}
-              resume={resume}
-              restart={restart}
-              setTimerHandler={setTimerHandler}
-              setTimerStart={setTimerStart}
-              timerStart={timerStart}
-            />
+          <div className="modifierBox">
+            <PomodoroApp />
+          </div>
+        )}
+
+        {/* for the todo list */}
+        <div className="modifier__icon">
+          <div className={`icon ${openTodo && "active"}`}>
+            <i
+              onClick={() => setOpenTodo(!openTodo)}
+              className="fas fa-tasks fa-2x"
+            ></i>
+          </div>
+        </div>
+        {openTodo && (
+          <div className="modifierBox">
             <h4>To do list</h4>
-            <TodoList /> */}
+            <TodoList todoList={todoList} addTodo={addTodo} />
           </div>
         )}
       </div>
